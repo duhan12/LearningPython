ca7ad0cb47e48ffb271745937f3056a1301d6410
Han Du <179272511@qq.com>
2019-08-17 18:30:31 +0100

    A simpler implementation

--- code/prime.py
+++ code/prime.py
@@ -1,13 +1,8 @@
-primenumber = list(range(2,1001))
-number_=[]
-
+prime = [ 2, 3 ]
 for i in range(2,1001):
-    for j in range(2,i):
-        if i%j==0:
-            number_.append(i)
-            break
-
-for l in number_:
-    if l in primenumber:
-        primenumber.remove(l)
-print(primenumber)
+    for j in range(2, i):
+        if i % j == 0: break;
+        elif j*j > i:
+            prime.append(i)
+            break;
+print(prime)
